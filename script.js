a2271d63678d1a45262b4e8eaadeaff1f527170b
Himeyorii ( Musa ) <musaaa41207@gmail.com>
2026-01-04 01:24:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// JAM SERVER
+function updateTime() {
+  const now = new Date();
+  const jam = document.getElementById("jam");
+  if (jam) {
+    jam.innerHTML = now.toLocaleTimeString();
+  }
+}
+setInterval(updateTime, 1000);
+updateTime();
+
+// NAVBAR ACTIVE OTOMATIS
+const navLinks = document.querySelectorAll(".nav-link");
+const currentPage = location.pathname.split("/").pop();
+
+navLinks.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
